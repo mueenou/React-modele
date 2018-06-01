425163fac45d07212b8bdc2b2021b565556704e9
Jean-Denis Vauguet <jd@vauguet.fr>
2018-06-01 12:31:57 +0200

    Utilisation des PropTypes

--- src/index.js
+++ src/index.js
@@ -1,19 +1,24 @@
 import React from 'react';
 import { render } from 'react-dom';
+import PropTypes from 'prop-types';
 
 // Notre premier composant React :)
 // 1. Création d'un élément virtuel (virtual DOM)
 // document.createElement('div')
 // React.createElement('div')
-const App = () => (
+const App = ({ text }) => (
   <div>
-    <p className="message">salut !</p>
+    <p className="message">{text}</p>
   </div>
 );
 
+App.propTypes = {
+  text: PropTypes.string.isRequired,
+};
+
 // 2. Ajout de l'élément virtuel dans le DOM du navigateur (mount/render).
 // cible.appendChild(quoiajouter)
-const rootComponent = App();
+const rootComponent = <App text="Salut React !" />;
 const targetNode = document.getElementById('root');
 render(rootComponent, targetNode);
 
