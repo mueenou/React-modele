b8d621cb11211d6b0bc31131560dd93035ffc0d0
Jean-Denis Vauguet <jd@vauguet.fr>
2018-06-04 21:47:28 +0200

    Composants React auto-suffisants
    Le JS et les styles de chaque composant sont co-localisés dans un
    dossier bien nommé, de sorte que chaque composant est réutilisable.

--- src/index.js
+++ src/index.js
@@ -1,23 +1,10 @@
 import 'babel-polyfill';
 import React from 'react';
 import { render } from 'react-dom';
-import PropTypes from 'prop-types';
 
-// Notre premier composant React :)
-// 1. Création d'un élément virtuel (virtual DOM)
-// document.createElement('div')
-// React.createElement('div')
-const App = ({ text }) => (
-  <div>
-    <p className="message">{text}</p>
-  </div>
-);
+import App from '~/components/App';
 
-App.propTypes = {
-  text: PropTypes.string.isRequired,
-};
-
-// 2. Ajout de l'élément virtuel dans le DOM du navigateur (mount/render).
+// Ajout de l'élément virtuel dans le DOM du navigateur (mount/render).
 // cible.appendChild(quoiajouter)
 document.addEventListener('DOMContentLoaded', () => {
   const rootComponent = <App text="Salut React !" />;
